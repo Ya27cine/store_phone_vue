4981acc3ea3706f6403ab6195e8fd35f3c2544e7
KHELIFA Yassine <prostam27@gmail.com>
2021-11-14 17:02:58 +0100

    install axios, getdata Smartphone

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import axios from 'axios'
+
+
+
+axios.defaults.baseURL = 'https://127.0.0.1:8000/api/';
 
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,49 @@
 <template>
-
-  
-
   <div class="home">
   <Navbar />
     <h2>Store Samphone</h2>
-  </div>
+    <button @click="getData" class="btn btn-info"> Get Data </button>
+<hr>
+</div>
+    <div class="row" v-if="smartphones" >
+        <div class="col-md-4" v-for="s in smartphones"> 
+              <ul>
+                <li> {{ s.Marque }}  {{ s.Name }} [ {{ s.Model }}] </li>
+              </ul>
+                
+             
+          </div>
+    </div>
+    <div v-else>
+      <h3> Loading ...</h3>
+    </div>
+  
+
+ 
 </template>
 
 <script>
 import Navbar from './../components/Navbar.vue'
+import axios from 'axios'
 
 export default {
   components: { Navbar },
-}
+    data() {
+      return {
+        smartphones: null
+     }
+    },
+    methods: {
+      getData(){
 
+          axios.get("smartphones").then((rep) => {
+
+              this.smartphones = rep.data['hydra:member'];
+              console.log(rep.data);
+          })
+
+      }
+    },
+  }
 
 </script>
