5db8ad2f6409c152df96fbefaa815c5e855eb7b6
KHELIFA Yassine <prostam27@gmail.com>
2021-11-15 00:06:09 +0100

    fetch all variant by id smartphone

---
+++ src/components/Variant.vue
@@ -0,0 +1,18 @@
+<template >
+<div class="card mx-4" style="width: 18rem;">
+        <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.clipartpanda.com%2Fandroid-smartphone-clipart-k8156167.jpg&f=1&nofb=1" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">ROM{{ data.rom}}, RAM {{ data.ram}}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make </p>
+            <router-link :to="{name: 'Show', params:{id: data.id }}" class="btn btn-primary">Detail</router-link>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['data'],
+}
+</script>
+<style lang="">
+    
+</style>
--- src/views/Smartphone/Show.vue
+++ src/views/Smartphone/Show.vue
@@ -1,33 +1,54 @@
 <template lang="">
     <div>
-        <div v-if="smartphone" class="row my-2">
-            <div class="col-md-10">
-                <h2>{{ smartphone.Marque }} {{ smartphone.Name }}</h2>
-                <p>
-                    {{ smartphone.stockSmartphones }}
-                </p>
+        <div v-if="variants" class="row my-2">
+            <h2 v-if="smartphone" >{{ smartphone.Marque }} {{ smartphone.Name }}</h2>
+            <div class="col-md-3"  v-for=" i in variants">           
+                <Variant  :data="i" />
             </div>
+                       
+        </div>
+        <div v-else>
+            <h2> Loading ....</h2>
         </div>
     </div>
+   
 </template>
 <script>
 
 import axios from 'axios'
+import Variant from './../../components/Variant.vue'
+
 export default {
     props: ['id'],
-
+    components:{
+        Variant
+    },
     data() {
         return {
-            smartphone: null
+            smartphone: [],
+            variants: []
         }
     },
     created() {
-        axios.get("smartphones/"+this.id).then( (rep) => {
-            this.smartphone =  rep.data 
-
-            console.log( rep.data)
-        })
+        this.getSmartphone();
+        this.getAllVariant()
     },
+
+    methods: {
+        async getAllVariant(){
+            await axios.get("smartphones/"+this.id+"/stock_smartphones").then( (rep) => {        
+                // console.log(rep.data['hydra:member'])
+                this.variants = rep.data['hydra:member']             
+            })
+        },   
+        
+        async getSmartphone(){
+            await axios.get("smartphones/"+this.id).then( (rep) => {                 
+                console.log(rep.data)
+                this.smartphone = rep.data                
+            })
+        },    
+    }
 }
 </script>
 <style lang="">
