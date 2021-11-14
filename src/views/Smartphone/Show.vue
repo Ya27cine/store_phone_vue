227579ab604e241d3e62e701056db1da598b90f5
KHELIFA Yassine <prostam27@gmail.com>
2021-11-14 19:14:10 +0100

    page Show: get variants

--- src/views/Smartphone/Show.vue
+++ src/views/Smartphone/Show.vue
@@ -1,11 +1,33 @@
 <template lang="">
     <div>
-        <h2 class="my-2">Show Product id : {{ id }}</h2>
+        <div v-if="smartphone" class="row my-2">
+            <div class="col-md-10">
+                <h2>{{ smartphone.Marque }} {{ smartphone.Name }}</h2>
+                <p>
+                    {{ smartphone.stockSmartphones }}
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+
+import axios from 'axios'
 export default {
-    props: ['id']
+    props: ['id'],
+
+    data() {
+        return {
+            smartphone: null
+        }
+    },
+    created() {
+        axios.get("smartphones/"+this.id).then( (rep) => {
+            this.smartphone =  rep.data 
+
+            console.log( rep.data)
+        })
+    },
 }
 </script>
 <style lang="">
