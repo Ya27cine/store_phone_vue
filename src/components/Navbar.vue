47b8be633744228e72d1d3ffd8e965ec623806a0
KHELIFA Yassine <prostam27@gmail.com>
2021-11-15 21:22:24 +0100

    Get Quantity for smartphone then show it

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,10 +5,13 @@
    
     <router-link class="navbar-brand" :to="{ name: 'Home' }">Samphone</router-link>
    
+    
     <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
         aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
+    
     <div class="collapse navbar-collapse" id="collapsibleNavId">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item active">
@@ -17,7 +20,7 @@
         
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" :to="{ name: 'Repair' }">Devis</router-link>
+          <router-link class="nav-link" :to="{ name: 'Home' }">Devis</router-link>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Accessoires</a>
@@ -38,5 +41,6 @@ export default {
 }
 </script>
 <style lang="">
-    
+
+
 </style>
--- src/components/Smartphone.vue
+++ src/components/Smartphone.vue
@@ -4,6 +4,8 @@
         <div class="card-body">
             <h5 class="card-title">{{ data.Marque}} {{ data.Name}}</h5>
             <p class="card-text">Some quick example text to build on the card title and make </p>
+            <h6> Quantity : <strong>{{ data.stockSmartphones.length }}</strong> </h6>
+             <h6> Prix : <strong> 90e - 110e</strong> </h6>
             <router-link :to="{name: 'Show', params:{id: data.id }}" class="btn btn-primary">Detail</router-link>
         </div>
     </div>
